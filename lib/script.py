cedcd9c7cff53e3f9d046dd74ec186715d667010
Laiba Sajid <laibasajid@Laibas-MacBook-Pro.local>
2024-10-16 16:00:28 -0400

    feat: iterate through states prompt user for input

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,21 @@
+import random
 from capitals import states
 
-print(states)
+print("Now playing: U.S. State Capitals Game Prototype")
+
+correct = []
+incorrect = []
+
+for state in states:
+    print(f"What is the capital of {state['name']}? press q to quit : )")
+    print(state)
+    userAnswer = input()
+    print(userAnswer)
+    if userAnswer.lower() == state['capital'].lower():
+        correct.append(state)
+        print("Great job you got it right!")
+    elif userAnswer == 'q':
+        break
+    else:
+        incorrect.append(state)
+        print("Uh-oh lets try this one again later!")
